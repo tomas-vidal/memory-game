9e9c6c85d236eb5e1d7acf409f3dc72dd9a54fe7
Tomás Vidal <103294839+tomas-vidal@users.noreply.github.com>
2023-07-11 15:18:19 -0300

    preparing files to start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Football Memory Game</h1>
+      <p>
+        Get points by clicking on an image but don't click on any more than
+        once!
+      </p>
     </div>
   );
 }
